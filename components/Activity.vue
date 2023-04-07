74ef536693869c6022c70c68339c60295c07dee8
500appsmounika <mounika.500apps@gmail.com>
2023-04-07 10:12:53 +0530

    Activities
    Signed-off-by: 500appsmounika <mounika.500apps@gmail.com>

---
+++ components/Activity.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="bg-white px-4 py-5 sm:px-6">
+      <TransitionRoot as="template" :show="open">
+    <Dialog as="div" class="relative z-10" @close="open = false">
+      <div class="fixed inset-0" />
+      <div class="fixed inset-0 overflow-hidden">
+        <div class="absolute inset-0 overflow-hidden">
+          <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+            <TransitionChild as="template" enter="transform transition ease-in-out duration-500 sm:duration-700" enter-from="translate-x-full" enter-to="translate-x-0" leave="transform transition ease-in-out duration-500 sm:duration-700" leave-from="translate-x-0" leave-to="translate-x-full">
+              <DialogPanel class="pointer-events-auto w-screen max-w-md">
+                <div class="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
+                  <div class="px-4 sm:px-6">
+                    <div class="flex items-start justify-between">
+                      <DialogTitle class="text-base font-semibold leading-6 text-gray-900">Activity</DialogTitle>
+                      <div class="ml-3 flex h-7 items-center">
+                        <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0" @click="open = false" ref="cancelButtonRef">Cancel</button>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="relative mt-6 flex-1 px-4 sm:px-6">
+                    <div class="min-w-0 flex-1">
+          <p class="text-sm font-semibold text-gray-900">
+            <a href="#" class="hover:underline">Mounika meesa</a>
+          </p>
+          <p class="text-sm font-semibold text-gray-900">
+            <a href="#" class="hover:underline">{{name}}</a>
+          </p>
+          <p class="text-sm text-gray-500">
+            <a href="#" class="hover:underline">{{ name }} added few minutes ago</a>
+          </p></div>
+                  </div>
+                </div>
+              </DialogPanel>
+            </TransitionChild>
+          </div>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
+  const props = defineProps<{
+  name: string
+  id:number
+}>()
+
+const open = ref(true)
+  </script>
---
+++ components/Websites.vue
@@ -0,0 +1,9 @@
+<template>
+    <Main v-bind="{url,addUrl}"/>
+   </template>
+   <script setup lang="ts">
+   // Declaring variables
+   const url="https://v1-orm-lib.mars.hipso.cc/activities/CONTACTS/1415?offset=0&limit=100&sort_column=id&sort_direction=desc"
+   const addUrl="https://v1-orm-lib.mars.hipso.cc/websites/"
+  
+   </script>
